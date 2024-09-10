c81cf523dda1e63ad1c7bf3ab514345cf94117d1
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2024-09-10 09:58:26 +0800

    Refactor FAQ  and Harga page component

--- src/app/(home)/faq/page.tsx
+++ src/app/(home)/faq/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
-import Header from '@/components/shared/Header';
-import Footer from '@/components/shared/Footer';
+import { cn } from '@/lib/utils';
+import { minervaModern } from '@/lib/fonts';
 
 const FAQSection = () => {
   const faqItems = [
@@ -15,30 +15,26 @@ const FAQSection = () => {
   ];
 
   return (
-    <>
-      <Header />
-      <div className='bg-surface mx-auto px-4 py-24'>
-          <h2 className="text-5xl mb-8 text-center">FREQUENTLY ASKED QUESTIONS</h2>
-          <div className="bg-surface p-6 rounded-lg max-w-2xl mx-auto">
-              <Accordion type="single" collapsible className="w-full shadow-none">
-                  {faqItems.map((item, index) => (
-                  <AccordionItem key={index} value={`item-${index}`} className='border-0'>
-                      <AccordionTrigger className="text-left hover:no-underline">
-                        <p>
-                          <span className='mr-2'>
-                            &bull;
-                          </span>
-                          {item.question}
-                        </p>
-                      </AccordionTrigger>
-                      <AccordionContent>{item.answer || "No answer provided."}</AccordionContent>
-                  </AccordionItem>
-                  ))}
-              </Accordion>
-          </div>
-      </div>
-      <Footer />
-    </>
+    <div className='bg-surface mx-auto px-4 py-24'>
+        <h2 className={cn('text-5xl mb-8 text-center', minervaModern.className)}>FREQUENTLY ASKED QUESTIONS</h2>
+        <div className="bg-surface p-6 rounded-lg max-w-2xl mx-auto">
+            <Accordion type="single" collapsible className="w-full shadow-none">
+                {faqItems.map((item, index) => (
+                <AccordionItem key={index} value={`item-${index}`} className='border-0'>
+                    <AccordionTrigger className="text-left hover:no-underline">
+                      <p>
+                        <span className='mr-2'>
+                          &bull;
+                        </span>
+                        {item.question}
+                      </p>
+                    </AccordionTrigger>
+                    <AccordionContent>{item.answer || "No answer provided."}</AccordionContent>
+                </AccordionItem>
+                ))}
+            </Accordion>
+        </div>
+    </div>
   );
 };
 
