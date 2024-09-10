0ccb5bc1ff89949faff1e8b17dd4d6c8829afe17
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2024-09-10 09:12:02 +0800

    Refactor FAQ page component

--- src/app/(home)/faq/page.tsx
+++ src/app/(home)/faq/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import Header from '@/components/shared/Header';
+import Footer from '@/components/shared/Footer';
 
 const FAQSection = () => {
   const faqItems = [
@@ -13,19 +15,30 @@ const FAQSection = () => {
   ];
 
   return (
-    <div className='bg-surface container mx-auto min-h-screen px-4 py-8'>
-        <h2 className="text-5xl mb-8 text-center">FREQUENTLY ASKED QUESTIONS</h2>
-        <div className="bg-surface p-6 rounded-lg max-w-2xl mx-auto">
-            <Accordion type="single" collapsible className="w-full shadow-none">
-                {faqItems.map((item, index) => (
-                <AccordionItem key={index} value={`item-${index}`}>
-                    <AccordionTrigger className="text-left">{item.question}</AccordionTrigger>
-                    <AccordionContent>{item.answer || "No answer provided."}</AccordionContent>
-                </AccordionItem>
-                ))}
-            </Accordion>
-        </div>
-    </div>
+    <>
+      <Header />
+      <div className='bg-surface mx-auto px-4 py-24'>
+          <h2 className="text-5xl mb-8 text-center">FREQUENTLY ASKED QUESTIONS</h2>
+          <div className="bg-surface p-6 rounded-lg max-w-2xl mx-auto">
+              <Accordion type="single" collapsible className="w-full shadow-none">
+                  {faqItems.map((item, index) => (
+                  <AccordionItem key={index} value={`item-${index}`} className='border-0'>
+                      <AccordionTrigger className="text-left hover:no-underline">
+                        <p>
+                          <span className='mr-2'>
+                            &bull;
+                          </span>
+                          {item.question}
+                        </p>
+                      </AccordionTrigger>
+                      <AccordionContent>{item.answer || "No answer provided."}</AccordionContent>
+                  </AccordionItem>
+                  ))}
+              </Accordion>
+          </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
