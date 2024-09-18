77c8c53051f571edb891fa135aef2be1bf448ac0
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2024-09-18 18:35:05 +0800

    Add Slicing create invitation user

--- src/app/(user)/my-invitation/page.tsx
+++ src/app/(user)/my-invitation/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react'
 
 export default function MyInvitationPage() {
@@ -6,9 +7,11 @@ export default function MyInvitationPage() {
         <div className="bg-surface flex flex-col items-center px-4 py-24">
             <div className="container mx-auto max-w-5xl">
                 <div className='text-center'>
-                    <Button className='mb-6' size='lg'>
-                        Buat Undangan Digital
-                    </Button>
+                    <Link href="/my-invitation/create">
+                        <Button className='mb-6' size='lg'>
+                            Buat Undangan Digital
+                        </Button>
+                    </Link>
                     <p>
                         Buat Undangan Digital Anda sekarang. Baik undangan pernikahan, undangan ulang tahun, atau undangan untuk kegiatan lain! Buat undangan yang memudahkan anda untuk berbagi informasi kepada tamu anda dan tentunya ramah lingkungan.
                     </p>
