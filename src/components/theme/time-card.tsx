63a1372f4c3199178416a8ca2718173eed676178
Dian Prasetyo <69192355+dianprsty@users.noreply.github.com>
2024-09-21 16:21:20 +0700

    Add theme 1 for desktop mode (#27)

--- src/components/theme/time-card.tsx
+++ src/components/theme/time-card.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Card } from '../ui/card';
+import React from "react";
+import { Card } from "../ui/card";
+import { cn } from "@/lib/utils";
 
 interface TimeCardProps {
   title: string;
   subtitle: string;
-  backgroundColorClass?: string;
-  textColorClass?: string;
-  borderClass?: string;
+  className?: string;
 }
 
 export default function TimeCard({
   title,
   subtitle,
-  backgroundColorClass = 'bg-phthalo-green',
-  textColorClass = 'text-white',
-  borderClass = 'border-none'
+  className,
 }: TimeCardProps) {
   return (
-    <Card className={`rounded-[100px] ${backgroundColorClass} ${textColorClass} ${borderClass}`}>
+    <Card
+      className={cn(
+        "rounded-[100px] text-white border-none bg-phthalo-green",
+        className
+      )}
+    >
       <div className="px-4 md:px-11 py-8 text-center">
         <h5 className="text-6xl font-bold mb-3">{title}</h5>
         <p className="text-3xl">{subtitle}</p>
---
+++ src/types/our-story-props.ts
@@ -0,0 +1,8 @@
+export type OurStoryProps = {
+  imgGroom: string;
+  imgBride: string;
+  quoteGroom: string;
+  quoteBride: string;
+  className?: string;
+  titleClassName?: string;
+};
