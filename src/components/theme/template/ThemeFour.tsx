04d30458e114758bb4c0f7e28e025e2ec76afef5
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2025-01-09 10:14:59 +0800

    Update slicing theme four for mobile responsive

--- src/components/theme/template/ThemeFour.tsx
+++ src/components/theme/template/ThemeFour.tsx
@@ -151,7 +151,11 @@ export default function ThemeFour({
             </div>
             <div className="rounded-[30px] overflow-hidden bg-isabelline min-h-60 aspect-[10/14]">
               <Image
-                src={manMedia?.[0]?.photo_url ? manMedia[0].photo_url : "/images/faceless/groom.png"}
+                src={
+                  manMedia?.[0]?.photo_url
+                    ? manMedia[0].photo_url
+                    : "/images/faceless/groom.png"
+                }
                 alt={"mempelai laki laki"}
                 width={509}
                 height={706}
@@ -160,7 +164,11 @@ export default function ThemeFour({
             </div>
             <div className="rounded-[30px] overflow-hidden bg-isabelline min-h-60 aspect-[10/14]">
               <Image
-                src={womanMedia?.[0]?.photo_url ? womanMedia[0].photo_url : "/images/faceless/bride.png"}
+                src={
+                  womanMedia?.[0]?.photo_url
+                    ? womanMedia[0].photo_url
+                    : "/images/faceless/bride.png"
+                }
                 alt={"mempelai perempuan"}
                 width={509}
                 height={706}
@@ -178,10 +186,10 @@ export default function ThemeFour({
             </div>
           </div>
         </section>
-        <section className=" flex flex-col md:flex-row justify-between items-center max-w-[1040px] mx-auto py-[44px] border-b border-b-raisin-black mb-10">
+        <section className=" flex flex-row justify-between items-center max-w-[1040px] mx-auto py-[44px] border-b border-b-raisin-black mb-10 px-6">
           <div className="flex flex-col gap-4 items-center">
             <p
-              className={cn("text-[40px]", inter.className)}
+              className={cn("text-2xl md:text-[40px]", inter.className)}
             >{`${prefixMan} ${nameMan} ${titleMan}`}</p>
             <p className={cn("text-base text-spanish-gray", inter.className)}>
               Bin {isTemplate ? "Abdul" : fatherMan}
@@ -198,7 +206,7 @@ export default function ThemeFour({
           </div>
           <div className="flex flex-col gap-4 items-center">
             <p
-              className={cn("text-[40px]", inter.className)}
+              className={cn("text-2xl md:text-[40px]", inter.className)}
             >{`${prefixWoman} ${nameWoman} ${titleWoman}`}</p>
             <p className={cn("text-base text-spanish-gray", inter.className)}>
               Binti {isTemplate ? "Ahmad" : fatherWoman}
@@ -206,114 +214,135 @@ export default function ThemeFour({
           </div>
         </section>
 
-        <section className="bg-fire-opal px-5 pt-5 relative">
-          <div className="border-b border-b-raisin-black max-w-[1040px] mx-auto pt-8 pb-[151px]">
-            <OurStory
-              {...story}
-              className="bg-white border border-raisin-black text-raisin-black "
+        <div className="flex flex-col">
+          <section className="bg-fire-opal px-5 pt-5 relative order-2">
+            <div className="border-b border-b-raisin-black max-w-[1040px] mx-auto pt-8 pb-[151px]">
+              <OurStory
+                {...story}
+                className="bg-white border border-raisin-black text-raisin-black "
+              />
+            </div>
+          </section>
+          <div className="relative container px-5 pt-[100px] pb-20 mx-auto max-w-desktop lg:px-20 xl:px-[100px] order-1">
+            <div className="block md:hidden mb-12 md:mb-0 text-center">
+              <h3 className="font-bold text-2xl mb-14">We’r Getting Married</h3>
+              <h2
+                className={cn(
+                  "text-xl text-dark-liver mb-3",
+                  allura.className
+                )}
+              >
+                Assalamualaikum Warrohmatullah Wabarrakatuhu
+              </h2>
+              <p className="font-medium text-spanish-gray text-lg mb-8">
+                By asking for the grace and blessing of Allah SWT. We intend to
+                hold a wedding celebration for our sons and daughters, which
+                Allah SWT willing will be held on:
+              </p>
+            </div>
+            <Image
+              src={"/images/icon/love-decor-theme-4.svg"}
+              alt={"love decoration"}
+              width={300}
+              height={550}
+              className="hidden lg:absolute -left-24 top-96 rotate-180"
             />
-          </div>
-        </section>
-        <div className="relative container px-5 pt-[100px] pb-20 mx-auto max-w-desktop lg:px-20 xl:px-[100px]">
-          <Image
-            src={"/images/icon/love-decor-theme-4.svg"}
-            alt={"love decoration"}
-            width={300}
-            height={550}
-            className="absolute -left-24 top-96 rotate-180"
-          />
-          <div className="mb-12 md:mb-16 lg:mb-24 ">
-            <h3 className="text-center text-gray text-3xl md:text-5xl mb-12 md:mb-36">
-              {ceremonyStartDateWithFullMonth}
-            </h3>
-            <DateCountDown
-              date={new Date(ceremonyStartDateTime)}
-              className="bg-fire-opal text-raisin-black w-full h-auto md:w-40 md:h-48"
-            />
-          </div>
-          <div className="mb-5 mx-auto max-w-[1040px]">
-            <CeremonyCard
-              backgroundImage="/images/background/bg-ceremony.png"
-              iconImage="/images/icon/wedding-ring.svg"
-              ceremonyTitle="Akad Ceremony"
-              ceremonyTime={`${ceremonyStartTime} ${
-                receptionTimezone ?? ""
-              } - ${
-                ceremonyEndTime
-                  ? ceremonyEndTime + " " + (receptionTimezone ?? "")
-                  : "Selesai"
-              }`}
-              locationTitle="InterContinental Jakarta Hotel"
-              locationAddress={
-                isTemplate ? "Jl.  Jalan Metro Pondok Indah" : ceremonyAddress
-              }
-              buttonText="Open Map"
-            />
-          </div>
-          <div className="mb-16 mx-auto max-w-[1040px]">
-            <CeremonyCard
-              backgroundImage="/images/background/bg-card-place.png"
-              iconImage="/images/icon/dinner-table.svg"
-              ceremonyTitle="Wedding Reseption"
-              ceremonyTime={`${receptionStartTime} ${
-                receptionTimezone ?? ""
-              } - ${
-                receptionEndTime
-                  ? receptionEndTime + " " + (receptionTimezone ?? "")
-                  : "Selesai"
-              }`}
-              locationTitle="InterContinental Jakarta Hotel"
-              locationAddress={
-                isTemplate ? "Jl.  Jalan Metro Pondok Indah" : receptionAddress
-              }
-              buttonText="Open Map"
-            />
-          </div>
+            <div className="mb-12 md:mb-16 lg:mb-24 ">
+              <h3 className="text-center text-gray text-3xl md:text-5xl mb-12 md:mb-36">
+                {ceremonyStartDateWithFullMonth}
+              </h3>
+              <DateCountDown
+                date={new Date(ceremonyStartDateTime)}
+                className="bg-fire-opal text-raisin-black w-full h-auto md:w-40 md:h-48"
+              />
+            </div>
+            <div className="mb-5 mx-auto max-w-[1040px]">
+              <CeremonyCard
+                backgroundImage="/images/background/bg-ceremony.png"
+                iconImage="/images/icon/wedding-ring.svg"
+                ceremonyTitle="Akad Ceremony"
+                ceremonyTime={`${ceremonyStartTime} ${
+                  receptionTimezone ?? ""
+                } - ${
+                  ceremonyEndTime
+                    ? ceremonyEndTime + " " + (receptionTimezone ?? "")
+                    : "Selesai"
+                }`}
+                locationTitle="InterContinental Jakarta Hotel"
+                locationAddress={
+                  isTemplate ? "Jl.  Jalan Metro Pondok Indah" : ceremonyAddress
+                }
+                buttonText="Open Map"
+              />
+            </div>
+            <div className="mb-16 mx-auto max-w-[1040px]">
+              <CeremonyCard
+                backgroundImage="/images/background/bg-card-place.png"
+                iconImage="/images/icon/dinner-table.svg"
+                ceremonyTitle="Wedding Reseption"
+                ceremonyTime={`${receptionStartTime} ${
+                  receptionTimezone ?? ""
+                } - ${
+                  receptionEndTime
+                    ? receptionEndTime + " " + (receptionTimezone ?? "")
+                    : "Selesai"
+                }`}
+                locationTitle="InterContinental Jakarta Hotel"
+                locationAddress={
+                  isTemplate
+                    ? "Jl.  Jalan Metro Pondok Indah"
+                    : receptionAddress
+                }
+                buttonText="Open Map"
+              />
+            </div>
 
-          <div
-            style={{
-              width: "100%",
-              height: "331px",
-            }}
-            className="relative mb-8 md:mb-16 max-w-[1040px] mx-auto rounded-[10px] overflow-hidden bg-stone-200 z-10"
-          >
-            <iframe
-              src={
-                isTemplate
-                  ? "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25290.95474578179!2d114.60485266193213!3d-3.3314483022037584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de423e3adcd9d9f%3A0x1b5ad295e2204466!2sSwiss-Belhotel%20Borneo%20Banjarmasin!5e0!3m2!1sid!2sid!4v1726833580741!5m2!1sid!2sid"
-                  : receptionLocation ?? ceremonyAddress
-              }
-              loading="lazy"
-              width="100%"
-              height="100%"
-            ></iframe>
-          </div>
-          <Image
-            src={"/images/icon/love-decor-theme-4.svg"}
-            alt={"love decoration"}
-            width={300}
-            height={550}
-            className="absolute w-24 md:w-[300px] bottom-0 right-0 md:-right-24 z-0"
-          />
+            <div
+              style={{
+                width: "100%",
+                height: "331px",
+              }}
+              className="relative mb-8 md:mb-16 max-w-[1040px] mx-auto rounded-[10px] overflow-hidden bg-stone-200 z-10"
+            >
+              <iframe
+                src={
+                  isTemplate
+                    ? "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25290.95474578179!2d114.60485266193213!3d-3.3314483022037584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de423e3adcd9d9f%3A0x1b5ad295e2204466!2sSwiss-Belhotel%20Borneo%20Banjarmasin!5e0!3m2!1sid!2sid!4v1726833580741!5m2!1sid!2sid"
+                    : receptionLocation ?? ceremonyAddress
+                }
+                loading="lazy"
+                width="100%"
+                height="100%"
+              ></iframe>
+            </div>
+            <Image
+              src={"/images/icon/love-decor-theme-4.svg"}
+              alt={"love decoration"}
+              width={300}
+              height={550}
+              className="hidden lg:absolute w-24 md:w-[300px] bottom-0 right-0 md:-right-24 z-0"
+            />
 
-          <div className="relative text-center mt-12 md:mt-20 mb-12 max-w-[1040px] mx-auto ">
-            <p className="text-center text-spanish-gray text-xl font-medium mb-16">
-              It is an honor and happiness for us if Mr / Ms / Brother (i) is
-              willing to attend to give blessings to the bride and groom.
-            </p>
-            <p className="text-spanish-gray text-xl font-medium mb-3">
-              &quot;And among His verses is that He has created for you wives of
-              your own kind, so that you may feel comfortable in them, and He
-              has made between you mawaddah and mercy. Verily in that are signs
-              for the people who think&quot;
-            </p>
-            <p className="text-granite-gray text-2xl font-medium">
-              - AR-RUM 21 -
-            </p>
+            <div className="relative text-center mt-12 md:mt-20 mb-12 max-w-[1040px] mx-auto ">
+              <p className="text-center text-spanish-gray text-xl font-medium mb-16">
+                It is an honor and happiness for us if Mr / Ms / Brother (i) is
+                willing to attend to give blessings to the bride and groom.
+              </p>
+              <p className="text-spanish-gray text-xl font-medium mb-3">
+                &quot;And among His verses is that He has created for you wives
+                of your own kind, so that you may feel comfortable in them, and
+                He has made between you mawaddah and mercy. Verily in that are
+                signs for the people who think&quot;
+              </p>
+              <p className="text-granite-gray text-2xl font-medium">
+                - AR-RUM 21 -
+              </p>
+            </div>
           </div>
         </div>
+
         <div className="container px-5 pt-[100px] pb-20 mx-auto max-w-desktop lg:px-20 xl:px-[100px]">
-          <div className="max-w-[1040px] mx-auto mb-[335px]">
+          <div className="max-w-[1040px] mx-auto mb-6 lg:mb-[335px]">
             <h3 className="text-black text-center text-4xl font-bold mb-8 lg:mb-16 ">
               Give a Gift
             </h3>
